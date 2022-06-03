0fdbd24ba640d00a31dd862f180cdbb831d275e3
Hadassa Moraes <96262130+hadmoraes@users.noreply.github.com>
2022-06-02 22:19:25 -0300

    create rout home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
-
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
+      <p>olaaaaa</p>
+      <BrowserRouter>
+        <Link to="/home">Home</Link>
+        <Routes>       
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="headerContainer">
+        <h1> Pedro's Pizzeria </h1>
+        <p> PIZZA TO FIT ANY TASTE</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

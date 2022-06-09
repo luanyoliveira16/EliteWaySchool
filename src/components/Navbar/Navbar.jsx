f6010997d45a0bd360680806edb0a8f61a3b4fbd
Hadassa Moraes <96262130+hadmoraes@users.noreply.github.com>
2022-06-09 15:03:14 -0300

    update Link in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 import styles from "./Navbar.module.css";
 import Logo from "../../assets/logo.png"
 
 const Navbar = () => {
   return <nav className={styles.navbar}>
-            <NavLink to = "/">
+            <Link to = "/">
               <img src={Logo}/>
-            </NavLink>
+            </Link>
             <ul className={styles.menu}>
-                      <a className={styles.opçoes}> <NavLink to = "/">Home</NavLink></a>
-                      <a className={styles.opçoes}> <NavLink to = "/docentes">Docentes</NavLink></a>
-                      <a className={styles.opçoes}> <NavLink to = "/cadastro">Cadastro</NavLink></a>
-                      <a className={styles.opçoes}> <NavLink to = "/contato">Contatos</NavLink></a>
+                      <a className={styles.opçoes}> <Link to = "/">Home</Link></a>
+                      <a className={styles.opçoes}> <Link to = "/docentes">Docentes</Link></a>
+                      <a className={styles.opçoes}> <Link to = "/cadastro">Cadastro</Link></a>
+                      <a className={styles.opçoes}> <Link to = "/contato">Contatos</Link></a>
             </ul>
   </nav>
 }
